03ba17765d95c0f85404fba3069de1d7de507c7d
Shaik090 <shaiknwmn99@gmail.com>
2025-03-23 21:46:49 +0530

    Form Validation Completed

--- README.md
+++ README.md
@@ -10,3 +10,16 @@ completion of styling index pages
 we can create new listing and also edit the previous listing
 
 data will be stored in mongoshell
+
+form validating is completed using the bootstrap form validation
+
+Custom error handling
+
+for custom error we can use
+
+>> app.use((err, req, res, next) { custom message });
+
+WrapAsync Error handler is used for form error handling
+
+next we can also use Express Custom Error
+
--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const Listing = require('./models/listing');
 const path = require('path');
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema.js");
 
 const MONGO_URL = 'mongodb://localhost:27017/wanderlust';
 
@@ -23,11 +26,23 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
+app.get("/", (req, res) => {
+    res.send("Welcome to WanderIn");
+});
+
+const validateListing = (req, res, next) => {
+    let {error} = listingSchema.validate(req.body);
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(",")
+        throw new ExpressError(400, errMsg);
+    }
+}
+
 // Index Route
-app.get("/listings", async (req, res) => {
+app.get("/listings", wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", {allListings});
-}); 
+})); 
 
 // New Route
 app.get("/listings/new", (req, res) => {
@@ -35,57 +50,67 @@ app.get("/listings/new", (req, res) => {
 });
 
 //create route
-app.post("/listings", async (req, res) => {
+app.post("/listings", validateListing, wrapAsync(async (req, res, next) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-});
+}));
 
 // Show Route
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapAsync(async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", {listing});
-});
+}));
 
 // Edit Route
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs", {listing});
-});
+}));
 
 // Update Route
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", validateListing, wrapAsync(async (req, res) => {
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect(`/listings/${id}`);    
-});
+}));
 
 // Delete Route
-app.delete("/listings/:id", async (req, res) => {
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
     let {id} = req.params;
     const deleteListing = await Listing.findByIdAndDelete(id);
     console.log(deleteListing);
     res.redirect("/listings");
-})
+}));
 
 // app.get("/testListing",async (req, res) => {
-//     // let sampleListings = new Listing({
-//     //     title: "My new Villa",
-//     //     description: "A beautiful villa by the beach",
-//     //     price: 1500,
-//     //     location: "Calangute, Goa",
-//     //     country: "India",
-//     // });
-//     // await sampleListings.save();
+    // let sampleListings = new Listing({
+    //     title: "My new Villa",
+    //     description: "A beautiful villa by the beach",
+    //     price: 1500,
+    //     location: "Calangate, Goa",
+    //     country: "India",
+    // });
+    // await sampleListings.save();
 //     console.log("Listing saved");
 //     res.send("Successfully testing");
 // });
 
+app.all("*", (req, res, next) => {
+    next(new ExpressError(404, "Page not found!"));
+});
+
+app.use((err, req, res, next) => {
+    let {statusCode=500, message="Something went wrong!"} = err;
+    res.status(statusCode).render("error.ejs", {message});
+    // res.status(statusCode).send(message);
+});
+
 app.get('/', (req, res) => {
     res.send("Hello! Welcome to Wanderlust");
-})
+});
 
 app.listen(8080, () =>{
     console.log('Server is running on port 8080');
